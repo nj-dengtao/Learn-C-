e20151b586008b4c961188ac01a6c3fe009ab614
DengTao <njdengtao@outlook.com>
2018-12-22 20:26:00 +0800

    第一个测试程序

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,26 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(277, 395);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 38);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "计算所得税";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(143, 377);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "0.0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 57);
+            this.label1.Location = new System.Drawing.Point(31, 377);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "My First GitHub Project";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "月平均工资额：";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +103,11 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,31 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double MonthlySalary = Convert.ToDouble(textBox1.Text);
+            double Threshold = 5000;
+            double Taxable = MonthlySalary - Threshold;
+            int Level = 0;
+            double[] TaxRate = new double[7] { 0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45 };
+            double[] Deduct = new double[7] { 0, 210, 1410, 2660, 4410, 7160, 15160 };
+            if (Taxable <= 3000) Level = 0;
+            if ((Taxable > 3000.01) && (Taxable <= 12000)) Level = 1;
+            if ((Taxable > 12000.01) && (Taxable <= 25000)) Level = 2;
+            if ((Taxable > 25000.01) && (Taxable <= 35000)) Level = 3;
+            if ((Taxable > 35000.01) && (Taxable <= 55000)) Level = 4;
+            if ((Taxable > 55000.01) && (Taxable <= 80000)) Level = 5;
+            if ((Taxable > 80000.01)) Level = 6;
+
+            if (Taxable < 0)
+            {
+                textBox2.Text = @"太扎心了，连交税资格都没有！";
+            }
+            else
+            {
+                textBox2.Text = Convert.ToString(Taxable * TaxRate[Level] - Deduct[Level]);
+            }
+        }
     }
 }
